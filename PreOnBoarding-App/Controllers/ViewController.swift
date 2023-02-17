ba3806f1642a562cdb957e094e3a377006005b0d
h-suo <phstoo1021@naver.com>
2023-02-17 22:59:27 +0900

    Commit loadImageFunc

--- PreOnBoarding-App/Controllers/ViewController.swift
+++ PreOnBoarding-App/Controllers/ViewController.swift
@@ -19,10 +19,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        detailView.loadAllButton.addTarget(self, action: #selector(allLoadButtonTapped), for: .touchUpInside)
+        detailView.loadAImageButton.addTarget(self, action: #selector(loadAButtonTapped), for: .touchUpInside)
+        detailView.loadBImageButton.addTarget(self, action: #selector(loadBButtonTapped), for: .touchUpInside)
+        detailView.loadCImageButton.addTarget(self, action: #selector(loadCButtonTapped), for: .touchUpInside)
+        detailView.loadDImageButton.addTarget(self, action: #selector(loadDButtonTapped), for: .touchUpInside)
+        detailView.loadEImageButton.addTarget(self, action: #selector(loadEButtonTapped), for: .touchUpInside)
+        
+        detailView.loadAllButton.addTarget(self, action: #selector(loadAllButtonTapped), for: .touchUpInside)
     }
     
-    @IBAction func allLoadButtonTapped(_ sender: UIButton) {
-        detailView.loadImageView.loadImage(link: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DF155EB6189BEC718D23EF4FA1A6348E51E9B5F7D84B3A994A9E2E2DC2B0B0FC/scale?width=1200&aspectRatio=1.78&format=jpeg")
+    @IBAction func loadAButtonTapped(_ sender: UIButton) {
+        detailView.loadAImageView.loadImage(link: "https://t2.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/13f9/image/vYfsHkNNLL2APvTd60uA7CBd7hg.jpg")
+    }
+    
+    @IBAction func loadBButtonTapped(_ sender: UIButton) {
+        detailView.loadBImageView.loadImage(link: "https://cdn.crowdpic.net/detail-thumb/thumb_d_D89EB4D319A77BDB461E5DEB9F793045.jpeg")
+    }
+    
+    @IBAction func loadCButtonTapped(_ sender: UIButton) {
+        detailView.loadCImageView.loadImage(link: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/German_Village_in_South_Korea_03.jpg/250px-German_Village_in_South_Korea_03.jpg")
+    }
+    
+    @IBAction func loadDButtonTapped(_ sender: UIButton) {
+        detailView.loadDImageView.loadImage(link: "https://cdn.britannica.com/29/150929-050-547070A1/lion-Kenya-Masai-Mara-National-Reserve.jpg")
+    }
+    
+    @IBAction func loadEButtonTapped(_ sender: UIButton) {
+        detailView.loadEImageView.loadImage(link: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DF155EB6189BEC718D23EF4FA1A6348E51E9B5F7D84B3A994A9E2E2DC2B0B0FC/scale?width=1200&aspectRatio=1.78&format=jpeg")
+    }
+    
+    @IBAction func loadAllButtonTapped(_ sender: UIButton) {
+        detailView.loadAImageView.loadImage(link: "https://t2.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/13f9/image/vYfsHkNNLL2APvTd60uA7CBd7hg.jpg")
+        detailView.loadBImageView.loadImage(link: "https://cdn.crowdpic.net/detail-thumb/thumb_d_D89EB4D319A77BDB461E5DEB9F793045.jpeg")
+        detailView.loadCImageView.loadImage(link: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/German_Village_in_South_Korea_03.jpg/250px-German_Village_in_South_Korea_03.jpg")
+        detailView.loadDImageView.loadImage(link: "https://cdn.britannica.com/29/150929-050-547070A1/lion-Kenya-Masai-Mara-National-Reserve.jpg")
+        detailView.loadEImageView.loadImage(link: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DF155EB6189BEC718D23EF4FA1A6348E51E9B5F7D84B3A994A9E2E2DC2B0B0FC/scale?width=1200&aspectRatio=1.78&format=jpeg")
     }
 }
--- PreOnBoarding-App/Views/MainView.swift
+++ PreOnBoarding-App/Views/MainView.swift
@@ -9,8 +9,14 @@ import UIKit
 import SnapKit
 
 class MainView: UIView {
+    // MARK: A
+    let stackViewA: UIStackView = {
+        let sv = UIStackView()
+        
+        return sv
+    }()
     
-    let loadImageView: UIImageView = {
+    let loadAImageView: UIImageView = {
         let iv = UIImageView()
         iv.translatesAutoresizingMaskIntoConstraints = false
         iv.image = UIImage(systemName: "photo")
@@ -19,14 +25,14 @@ class MainView: UIView {
         return iv
     }()
     
-    let progressView: UIProgressView = {
+    let progressViewA: UIProgressView = {
         let pv = UIProgressView()
         pv.progress = 0.5
         
         return pv
     }()
     
-    let loadImageButton: UIButton = {
+    let loadAImageButton: UIButton = {
         let bt = UIButton(type: .system)
         bt.setTitle("Load", for: .normal)
         bt.setTitleColor(.white, for: .normal)
@@ -36,11 +42,137 @@ class MainView: UIView {
         
         return bt
     }()
-    
-    let stackView: UIStackView = {
+    // MARK: B
+    let stackViewB: UIStackView = {
         let sv = UIStackView()
         
         return sv
+    }()
+    
+    let loadBImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.image = UIImage(systemName: "photo")
+        iv.tintColor = .tintColor
+        
+        return iv
+    }()
+    
+    let progressViewB: UIProgressView = {
+        let pv = UIProgressView()
+        pv.progress = 0.5
+        
+        return pv
+    }()
+    
+    let loadBImageButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("Load", for: .normal)
+        bt.setTitleColor(.white, for: .normal)
+        bt.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        bt.backgroundColor = .tintColor
+        bt.layer.cornerRadius = 4
+        
+        return bt
+    }()
+    // MARK: C
+    let stackViewC: UIStackView = {
+        let sv = UIStackView()
+        
+        return sv
+    }()
+    
+    let loadCImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.image = UIImage(systemName: "photo")
+        iv.tintColor = .tintColor
+        
+        return iv
+    }()
+    
+    let progressViewC: UIProgressView = {
+        let pv = UIProgressView()
+        pv.progress = 0.5
+        
+        return pv
+    }()
+    
+    let loadCImageButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("Load", for: .normal)
+        bt.setTitleColor(.white, for: .normal)
+        bt.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        bt.backgroundColor = .tintColor
+        bt.layer.cornerRadius = 4
+        
+        return bt
+    }()
+    // MARK: D
+    let stackViewD: UIStackView = {
+        let sv = UIStackView()
+        
+        return sv
+    }()
+    
+    let loadDImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.image = UIImage(systemName: "photo")
+        iv.tintColor = .tintColor
+        
+        return iv
+    }()
+    
+    let progressViewD: UIProgressView = {
+        let pv = UIProgressView()
+        pv.progress = 0.5
+        
+        return pv
+    }()
+    
+    let loadDImageButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("Load", for: .normal)
+        bt.setTitleColor(.white, for: .normal)
+        bt.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        bt.backgroundColor = .tintColor
+        bt.layer.cornerRadius = 4
+        
+        return bt
+    }()
+    // MARK: E
+    let stackViewE: UIStackView = {
+        let sv = UIStackView()
+        
+        return sv
+    }()
+    
+    let loadEImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.image = UIImage(systemName: "photo")
+        iv.tintColor = .tintColor
+        
+        return iv
+    }()
+    
+    let progressViewE: UIProgressView = {
+        let pv = UIProgressView()
+        pv.progress = 0.5
+        
+        return pv
+    }()
+    
+    let loadEImageButton: UIButton = {
+        let bt = UIButton(type: .system)
+        bt.setTitle("Load", for: .normal)
+        bt.setTitleColor(.white, for: .normal)
+        bt.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        bt.backgroundColor = .tintColor
+        bt.layer.cornerRadius = 4
+        
+        return bt
     }()
     
     let loadAllButton: UIButton = {
@@ -62,10 +194,31 @@ class MainView: UIView {
     
     func setupView() {
         
-        self.stackView.addSubview(loadImageView)
-        self.stackView.addSubview(progressView)
-        self.stackView.addSubview(loadImageButton)
-        self.addSubview(stackView)
+        self.stackViewA.addSubview(loadAImageView)
+        self.stackViewA.addSubview(progressViewA)
+        self.stackViewA.addSubview(loadAImageButton)
+        
+        self.stackViewB.addSubview(loadBImageView)
+        self.stackViewB.addSubview(progressViewB)
+        self.stackViewB.addSubview(loadBImageButton)
+        
+        self.stackViewC.addSubview(loadCImageView)
+        self.stackViewC.addSubview(progressViewC)
+        self.stackViewC.addSubview(loadCImageButton)
+        
+        self.stackViewD.addSubview(loadDImageView)
+        self.stackViewD.addSubview(progressViewD)
+        self.stackViewD.addSubview(loadDImageButton)
+        
+        self.stackViewE.addSubview(loadEImageView)
+        self.stackViewE.addSubview(progressViewE)
+        self.stackViewE.addSubview(loadEImageButton)
+        
+        self.addSubview(stackViewA)
+        self.addSubview(stackViewB)
+        self.addSubview(stackViewC)
+        self.addSubview(stackViewD)
+        self.addSubview(stackViewE)
         self.addSubview(loadAllButton)
         
         setConstraints()
@@ -76,32 +229,125 @@ class MainView: UIView {
     }
     
     func setConstraints() {
-        stackView.snp.makeConstraints { make in
+        stackViewA.snp.makeConstraints { make in
             make.trailing.equalToSuperview()
             make.leading.equalToSuperview()
             make.top.equalTo(safeAreaLayoutGuide)
             make.height.equalTo(100)
         }
-        loadImageView.snp.makeConstraints { make in
-            make.centerY.equalTo(stackView)
-            make.leading.equalTo(stackView.snp.leading).offset(20)
+        loadAImageView.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewA)
+            make.leading.equalTo(stackViewA.snp.leading).offset(20)
             make.width.equalTo(120)
             make.height.equalTo(90)
         }
-        progressView.snp.makeConstraints { make in
-            make.centerY.equalTo(stackView)
-            make.leading.equalTo(loadImageView.snp.trailing).offset(4)
+        progressViewA.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewA)
+            make.leading.equalTo(loadAImageView.snp.trailing).offset(4)
         }
-        loadImageButton.snp.makeConstraints { make in
-            make.centerY.equalTo(stackView)
-            make.leading.equalTo(progressView.snp.trailing)
+        loadAImageButton.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewA)
+            make.leading.equalTo(progressViewA.snp.trailing)
             make.width.equalTo(80)
-            make.trailing.equalTo(stackView.snp.trailing).offset(-20)
+            make.trailing.equalTo(stackViewA.snp.trailing).offset(-20)
         }
+        
+        stackViewB.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.top.equalTo(stackViewA.snp.bottom)
+            make.height.equalTo(100)
+        }
+        loadBImageView.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewB)
+            make.leading.equalTo(stackViewB.snp.leading).offset(20)
+            make.width.equalTo(120)
+            make.height.equalTo(90)
+        }
+        progressViewB.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewB)
+            make.leading.equalTo(loadBImageView.snp.trailing).offset(4)
+        }
+        loadBImageButton.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewB)
+            make.leading.equalTo(progressViewB.snp.trailing)
+            make.width.equalTo(80)
+            make.trailing.equalTo(stackViewB.snp.trailing).offset(-20)
+        }
+        
+        stackViewC.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.top.equalTo(stackViewB.snp.bottom)
+            make.height.equalTo(100)
+        }
+        loadCImageView.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewC)
+            make.leading.equalTo(stackViewC.snp.leading).offset(20)
+            make.width.equalTo(120)
+            make.height.equalTo(90)
+        }
+        progressViewC.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewC)
+            make.leading.equalTo(loadCImageView.snp.trailing).offset(4)
+        }
+        loadCImageButton.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewC)
+            make.leading.equalTo(progressViewC.snp.trailing)
+            make.width.equalTo(80)
+            make.trailing.equalTo(stackViewC.snp.trailing).offset(-20)
+        }
+        
+        stackViewD.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.top.equalTo(stackViewC.snp.bottom)
+            make.height.equalTo(100)
+        }
+        loadDImageView.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewD)
+            make.leading.equalTo(stackViewD.snp.leading).offset(20)
+            make.width.equalTo(120)
+            make.height.equalTo(90)
+        }
+        progressViewD.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewD)
+            make.leading.equalTo(loadDImageView.snp.trailing).offset(4)
+        }
+        loadDImageButton.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewD)
+            make.leading.equalTo(progressViewD.snp.trailing)
+            make.width.equalTo(80)
+            make.trailing.equalTo(stackViewD.snp.trailing).offset(-20)
+        }
+        
+        stackViewE.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.top.equalTo(stackViewD.snp.bottom)
+            make.height.equalTo(100)
+        }
+        loadEImageView.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewE)
+            make.leading.equalTo(stackViewE.snp.leading).offset(20)
+            make.width.equalTo(120)
+            make.height.equalTo(90)
+        }
+        progressViewE.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewE)
+            make.leading.equalTo(loadEImageView.snp.trailing).offset(4)
+        }
+        loadEImageButton.snp.makeConstraints { make in
+            make.centerY.equalTo(stackViewE)
+            make.leading.equalTo(progressViewE.snp.trailing)
+            make.width.equalTo(80)
+            make.trailing.equalTo(stackViewE.snp.trailing).offset(-20)
+        }
+        
         loadAllButton.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
-            make.top.equalTo(stackView.snp.bottom).offset(20)
+            make.top.equalTo(stackViewE.snp.bottom).offset(20)
         }
     }
 }
