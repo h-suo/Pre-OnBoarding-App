045382d26ee7386fa35c3366a69206c39e2710c7
h-suo <phstoo1021@naver.com>
2023-02-18 10:42:07 +0900

    Initial Commit

---
+++ PreOnBoarding-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreOnBoarding-Project
+//
+//  Created by 표현수 on 2023/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
