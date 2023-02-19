fde248fcd160e8db26eea45abd0baf93a6ecc308
h-suo <phstoo1021@naver.com>
2023-02-19 12:32:02 +0900

    Commit code

--- PreOnBoarding-Project/Image/LoadImage.swift
+++ PreOnBoarding-Project/Image/LoadImage.swift
@@ -7,45 +7,45 @@
 
 import UIKit
 
-//extension UIImageView {
-//    func loadImage(link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-//        guard let url = URL(string: link) else { return }
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "GET"
-//
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            guard
-//                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-//                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-//                let data = data, error == nil,
-//                let image = UIImage(data: data)
-//                else {
-//                    print("Load image fail")
-//                    return
-//            }
-//
-//            DispatchQueue.main.async() { [weak self] in
-//                print("Load image success")
-//
-//                self?.contentMode = mode
-//                self?.image = image
-//            }
-//        }.resume()
-//    }
-//}
-
 extension UIImageView {
-    func loadImage(link: String) {
-        DispatchQueue.global().async { [weak self] in
-            guard let url = URL(string: link) else { return }
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.contentMode = .scaleAspectFit
-                        self?.image = image
-                    }
-                }
+    func loadImage(link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else {
+                    print("Load image fail")
+                    return
             }
-        }
+
+            DispatchQueue.main.async() { [weak self] in
+                print("Load image success")
+
+                self?.contentMode = mode
+                self?.image = image
+            }
+        }.resume()
     }
 }
+
+//extension UIImageView {
+//    func loadImage(link: String) {
+//        DispatchQueue.global().async { [weak self] in
+//            guard let url = URL(string: link) else { return }
+//            if let data = try? Data(contentsOf: url) {
+//                if let image = UIImage(data: data) {
+//                    DispatchQueue.main.async {
+//                        self?.contentMode = .scaleAspectFit
+//                        self?.image = image
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
